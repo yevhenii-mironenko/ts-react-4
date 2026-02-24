c59492d8bcaabef0453091fecc141d0df3f7e287
Yevhenii Myronenko <jackmironenko@gmail.com>
2026-02-24 20:59:03 +0200

    feat: implement data fetching and display blog posts

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,41 @@
+import { useEffect, useState } from "react";
+import { get } from "./util/http";
+import BlogPosts, { BlogPost } from "./components/BlogPosts";
+
 function App() {
-  return <h1>Data Fetching!</h1>;
+  const [fetchedData, setFetchedData] = useState<BlogPost[]>([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = (await get(
+        "https://jsonplaceholder.typicode.com/posts/",
+      )) as RawDataBlogPost[];
+
+      const blogPosts: BlogPost[] = data.map((post) => ({
+        id: post.id,
+        title: post.title,
+        text: post.body,
+      }));
+
+      setFetchedData(blogPosts);
+    }
+
+    fetchData();
+  }, []);
+
+  return (
+    <main>
+      <img src="/data-fetching.png" alt="Data Fetching" />
+      <BlogPosts posts={fetchedData} />
+    </main>
+  );
 }
 
 export default App;
+
+type RawDataBlogPost = {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+};

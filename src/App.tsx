8f96dfda0f816840b2e7267c14bf925ea8abb099
Yevhenii Myronenko <jackmironenko@gmail.com>
2026-02-26 21:47:37 +0200

    feat: add Zod validation and improve data fetching in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,55 @@
 import { useEffect, useState } from "react";
 import { get } from "./util/http";
+import { z } from "zod";
 import BlogPosts, { BlogPost } from "./components/BlogPosts";
 
 function App() {
   const [fetchedData, setFetchedData] = useState<BlogPost[]>([]);
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    async function fetchData() {
-      const data = (await get(
-        "https://jsonplaceholder.typicode.com/posts/",
-      )) as RawDataBlogPost[];
-
-      const blogPosts: BlogPost[] = data.map((post) => ({
-        id: post.id,
-        title: post.title,
-        text: post.body,
-      }));
-
-      setFetchedData(blogPosts);
+    async function fetchPosts() {
+      setIsFetching(true);
+      try {
+        const data = await get("https://jsonplaceholder.typicode.com/posts");
+        const parsedData = expectedResponseDataSchema.parse(data);
+        const blogPosts: BlogPost[] = parsedData.map((rawPost) => {
+          return {
+            id: rawPost.id,
+            title: rawPost.title,
+            text: rawPost.body,
+          };
+        });
+        setFetchedData(blogPosts);
+      } catch (error) {
+        if (error instanceof Error) {
+          setError(error.message);
+        }
+      } finally {
+        setIsFetching(false);
+      }
     }
-
-    fetchData();
+    fetchPosts();
   }, []);
 
   return (
     <main>
       <img src="/data-fetching.png" alt="Data Fetching" />
       <BlogPosts posts={fetchedData} />
+      {isFetching && <p>Fetching posts...</p>}
+      {error && <p id="error">{error}</p>}
     </main>
   );
 }
 
 export default App;
 
-type RawDataBlogPost = {
-  id: number;
-  userId: number;
-  title: string;
-  body: string;
-};
+const rawDataBlogPostSchema = z.object({
+  id: z.number(),
+  userId: z.number(),
+  title: z.string(),
+  body: z.string(),
+});
+
+const expectedResponseDataSchema = z.array(rawDataBlogPostSchema);
